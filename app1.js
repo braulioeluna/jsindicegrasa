f1951c76846ce778d8ef416acc129a97d29639f5
braulioeluna <braulioemiliomtzlun@gmail.com>
2024-08-27 18:40:59 -0600

    listo

---
+++ app1.js
@@ -0,0 +1,7 @@
+
+let numero1 = prompt("Ingresa tu peso en kg");
+let numero2 = prompt("Ingresa tu estatura en metros");
+let resultado =  parseInt( numero1) / parseFloat( numero2**2); 
+alert ("Tu indice de grasa corporal es: " + resultado);
+
+
